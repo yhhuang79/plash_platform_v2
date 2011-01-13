3eff11127a5b6e64318731698d05492bcf3676ee
Yu-Hsiang Huang <yhhuang79@gmail.com>
2011-01-13 07:58:41 +0000

    add javadoc comment add param check

--- src/tw/edu/sinica/iis/ants/components/getUserTripIdComponent.java
+++ src/tw/edu/sinica/iis/ants/components/getUserTripIdComponent.java
@@ -34,25 +34,42 @@ public class getUserTripIdComponent {
         /*
          * Please Implement Your Programming Logic From Here 
          */
+        
+        /**
+         * return whole trip ids for a user
+         * 
+         * @author Yu-Hsiang Huang 
+         * @version 1.0, 01/10/2010
+         * @param     userid 
+         * @return    tripid_list 
+         * @see       connpost.java
+         */
 
-       
-       
-        Session session = sessionFactory.openSession();
-
-        Criteria criteria = session.createCriteria(T_UserPointLocationTime.class);
-        criteria.add(Restrictions.eq("userid", Integer.parseInt(map.get("userid").toString())));
-        List tripid_list = new ArrayList();
-        Iterator tripids = criteria.list().iterator();
-        while(tripids.hasNext()) {
-            T_UserPointLocationTime tripid = (T_UserPointLocationTime) tripids.next();
-            tripid_list.add(tripid.getTrip_id());
-        }
+		Session session = sessionFactory.openSession();
+        
+        Integer userid = null;
+        
+        if (map.containsKey("userid")) {
+			userid = Integer.parseInt(map.get("userid").toString());
+		}
+        
+        if (userid.equals("")) {
+			map.put("message", "userid is empty");
+		} else {   
+			Criteria criteria = session.createCriteria(T_UserPointLocationTime.class);
+			criteria.add(Restrictions.eq("userid", Integer.parseInt(map.get("userid").toString())));
+			List tripid_list = new ArrayList();
+			Iterator tripids = criteria.list().iterator();
+			while(tripids.hasNext()) {
+				T_UserPointLocationTime tripid = (T_UserPointLocationTime) tripids.next();
+				tripid_list.add(tripid.getTrip_id());
+			}
  
+			map.put("tripid_list", tripid_list);
+		}
+        
         session.close();
        
-        map.put("tripid_list", tripid_list);
-       
-
         /*
          * End of Programming Logic Implementation
          */
