55f719a84072d767bd19a120db6c807d82415e4c
Kenny	Wu <ycw@iis.sinica.edu.tw>
2011-05-12 09:40:02 +0000

    change input from List to CSV

--- src/tw/edu/sinica/iis/ants/components/getAllFriendLocationComponent.java
+++ src/tw/edu/sinica/iis/ants/components/getAllFriendLocationComponent.java
@@ -44,6 +44,18 @@ public class getAllFriendLocationComponent {
 		System.out.println("PeopleLocations Start:\t"+ Calendar.getInstance().getTimeInMillis());
 		Session session = sessionFactory.openSession(); 
 
+		
+		
+		String useridSet = (String) map.get("friend_list");
+		
+		String[] useridArray = useridSet.split(",");
+		for(int i=0; i<useridArray.length; i++){
+			if(!useridArray[i].trim().matches("[0-9]+")){
+				return map;
+			}
+		}
+		
+		/*
 		List friend_list = (List) map.get("friend_list");
 		Iterator i = friend_list.iterator();
 		String useridSet = "";
@@ -51,7 +63,7 @@ public class getAllFriendLocationComponent {
 			useridSet += i.next().toString()+",";
 		}
 		useridSet = "(" + useridSet.substring(0, useridSet.length()-1) + ")";
-		
+		*/
 		
 		String sql = "select ST_AsText(gps) as gpsLoc, timestamp, userid from user_location.user_point_location_time uplt where id in(select   max(id) from user_location.user_point_location_time where (trip_id, userid) in (select  max(trip_id) as trip_id,userid from user_location.user_point_location_time where userid in "+useridSet+" group by userid ) group by trip_id, userid);";
 		// 建立 SQLQuery
