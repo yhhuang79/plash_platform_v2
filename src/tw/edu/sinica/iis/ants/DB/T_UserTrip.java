87fe9120f9b8a84ff807ab26620ed84e9912b9fc
Kenny	Wu <ycw@iis.sinica.edu.tw>
2011-06-17 07:48:37 +0000

    add user_trip table class

---
+++ src/tw/edu/sinica/iis/ants/DB/T_UserTrip.java
@@ -0,0 +1,37 @@
+package tw.edu.sinica.iis.ants.DB;
+
+public class T_UserTrip {
+	private int id;
+	private int userid;
+	private int trip_count;
+	
+	public T_UserTrip(){
+	
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public int getTrip_count() {
+		return trip_count;
+	}
+
+	public void setTrip_count(int tripCount) {
+		trip_count = tripCount;
+	}
+
+	
+}
