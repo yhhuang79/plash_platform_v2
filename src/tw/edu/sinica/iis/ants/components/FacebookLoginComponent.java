23c7bb9d49df454839918303aa0c0de8e821459c
Yu-Hsiang Huang <yhhuang79@gmail.com>
2013-10-20 19:53:48 +0000

    New Facebooklogin

--- src/tw/edu/sinica/iis/ants/components/FacebookLoginComponent.java
+++ src/tw/edu/sinica/iis/ants/components/FacebookLoginComponent.java
@@ -4,10 +4,10 @@ import java.util.Calendar;
 import java.util.Iterator;
 import java.util.Map;
 
-
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
 import tw.edu.sinica.iis.ants.sendMail;
@@ -32,16 +32,14 @@ public class FacebookLoginComponent {
 	public Object greet(Map map) {
 		System.out.println("FacebookLogin Start:\t"+ Calendar.getInstance().getTimeInMillis());
 
-		Session session = sessionFactory.openSession(); 
+		Session session = sessionFactory.openSession();
+		String username = map.get("username").toString();
 		String email = map.get("email").toString();
 		String facebookid = map.get("facebookid").toString();
-		//String password = map.remove("password").toString();
-		//String password = map.get("password").toString();
 		
 		Criteria criteria = session.createCriteria(T_Login.class);
 		criteria.add(Restrictions.eq("email", email));
 		criteria.add(Restrictions.eq("facebookid", facebookid));
-		//criteria.add(Restrictions.eq("password", password));
 		System.out.println("FacebookLogin Component:  " + email +" : "+ facebookid);
 		Iterator users = criteria.list().iterator(); 
 		if(users.hasNext()) {
@@ -51,21 +49,30 @@ public class FacebookLoginComponent {
 				map.put("sid", user.getSid());
 				String message = "Successful Login:" + user.getSid();
 				map.put("message", message);
-				
-				//TODO: String aa = WebSiteCore.getUserLatestTrip(userid);
-				//		req.setAttribute("map", aa);
-				
-			}
-			//account is not activate
-			else {
+			} else {
 				map.put("sid", "0");
 				map.put("message", "Inactivate");
 			}
 
-		}
-		else{
-			map.put("sid", "0");
-			map.put("message", "Login Fail");
+		} else {
+			//map.put("sid", "0");
+			//map.put("message", "Login Fail");
+			T_Login user = new T_Login();
+			
+			user.setUsername(username);
+			user.setEmail(email);
+			user.setPassword("");
+			user.setPasscode("");
+			user.setFacebookid(facebookid);
+			user.setConfirmed(true);
+			
+			Transaction tx = session.beginTransaction();
+			session.persist(user);
+			tx.commit();
+			
+			map.put("sid", user.getSid());
+			String message = "Successful Login:" + user.getSid();
+			map.put("message", message);			
 		}
  
 		session.close();
